8047bff0104727fe25353d479f346946511b3fc0
HimanshuAgarwal136 <124384911+HimanshuAgarwal136@users.noreply.github.com>
2025-08-20 14:10:00 +0530

    feat: Added public pages but still needed to be fixed

--- src/app.js
+++ src/app.js
@@ -2,13 +2,13 @@
 import express from 'express'
 import urlRoutes from './routes/urlRoutes.js'
 import { limiter } from './middleware/rateLimiter.js';
+import path from 'path'
 
 
-// const express = require('express')
-// const urlRoutes = require('./routes/urlRoutes');
 
 const app = express();
 app.use(express.json());
+app.use(express.static(path.resolve('public')));
 app.use('/', urlRoutes);
 
 export default app
